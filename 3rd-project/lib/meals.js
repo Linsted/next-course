a2153bfc0402413d9c6d2b54cf450bd065ec9e64
Linsted <ivan.malenko18@gmail.com>
2024-02-03 20:39:31 +0200

    feat: add serve actions

--- 3rd-project/lib/meals.js
+++ 3rd-project/lib/meals.js
@@ -1,9 +1,42 @@
 import sql from "better-sqlite3";
+import slugify from "slugify";
+import xss from "xss";
+import fs from "node:fs";
 
 const db = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
+  // throw new Error("Failed to fetch meals");
+
   return db.prepare("SELECT * FROM meals").all();
+}
+
+export function getMealById(slug) {
+  return db.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
+}
+
+export async function saveMeal(meal) {
+  meal.slug = slugify(meal.title, { lower: true });
+  meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${meal.slug}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferImage), (error) => {
+    if (error) {
+      throw new Error("Failed to save image");
+    }
+
+    meal.image = `/images/${fileName}`;
+
+    db.prepare(
+      `INSERT INTO meals (slug, title, summary, instructions, image, creator, creator_email) 
+      VALUES (@slug, @title, @summary, @instructions, @image, @creator, @creator_email)`
+    ).run(meal);
+  });
 }
